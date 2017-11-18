8631b7404429f09fa2beabfc2f4b19a3e21fad20
Sergey Dyubov <greypks@gmail.com>
2017-11-18 12:29:31 +0600

    changed .gitignore

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'pizzaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Antarctica/Vostok'
 
 USE_I18N = True
 
--- pizzashop/models.py
+++ pizzashop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Dish(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.IntegerField(default=0)
+
+
