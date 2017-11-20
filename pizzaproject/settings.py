eb69d357d6b79d7b3bd7722ab6f2bae4e0edd838
Sergey Dyubov <greypks@gmail.com>
2017-11-20 18:52:10 +0600

    Blue admin

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.theme.red',
+    'material.theme.lightblue',
     'material',
     'material.frontend',
     'material.admin',
