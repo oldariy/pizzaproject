c6237ea4cfd02876113810ed5a5f75766307b5a2
Sergey Dyubov <greypks@gmail.com>
2017-11-20 20:21:58 +0600

    Add item page

--- pizzashop/views.py
+++ pizzashop/views.py
@@ -7,3 +7,8 @@ from django.http import HttpResponse
 def index(request):
     items = Item.objects.filter()
     return render(request, 'pizzashop/index.html', locals())
+
+
+def item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    return render(request, 'pizzashop/item.html', locals())
