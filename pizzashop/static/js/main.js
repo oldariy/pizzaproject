2892ca4f2bd67f4fe2e783bf88ddc85769777a73
greypk <greypks@gmail.com>
2017-12-01 01:37:41 +0600

    Add item page

---
+++ pizzashop/static/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+	'use strict';
+	let orderForm = document.querySelector('#order-form');
+	orderForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let count = document.querySelector('#count');
+    console.log(count);
+    let itemName = document.querySelector('#submit-btn').getAttribute('data-name');
+    console.log(itemName);
+	});
+	
+};
