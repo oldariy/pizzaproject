b80192c33ffa5d94d5b0c7fbc4aaab9eddc8579b
greypk <greypks@gmail.com>
2017-12-03 14:16:00 +0600

    Add basket

--- pizzashop/static/js/main.js
+++ pizzashop/static/js/main.js
@@ -1,13 +1,21 @@
-window.onload = function () {
-	'use strict';
-
-	let orderForm = document.querySelector('#order-form');
-	orderForm.addEventListener('submit', function(e) {
+$(document).ready(function () {
+    // orderForm
+	let orderForm = $('#order-form');
+	orderForm.on('submit', function (e) {
         e.preventDefault();
-        let count = document.querySelector('#count');
-        console.log(count);
-        let itemName = document.querySelector('#submit-btn').getAttribute('data-name');
-        console.log(itemName);
-	});
-	
-};
+        let count = $('#count').val();
+        let submitBtn = $('#submit-btn');
+        let itemId = submitBtn.data('item_id');
+        let itemName = submitBtn.data('name');
+        let itemPrice = submitBtn.data('price');
+        console.log(itemId + ' ' + itemName + ' ' + itemPrice);
+
+        $('.basket__ul').append('<li class="basket__item">' + itemName + ' ' + count + ' ' + 'шт. ' + (itemPrice *  count) + ' руб.</li>');
+
+    });
+
+	$('.basket__text').on('click', function (e) {
+        e.preventDefault();
+        $('.basket').toggleClass('hidden');
+    });
+});
