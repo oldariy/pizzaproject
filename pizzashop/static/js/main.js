d9dad81313ff09e624890eb648acc9d6589d3eb5
Sergey Dyubov <greypks@gmail.com>
2017-12-05 22:36:34 +0600

    Change basket to dialog

--- pizzashop/static/js/main.js
+++ pizzashop/static/js/main.js
@@ -94,4 +94,16 @@ $(document).ready(function () {
 			}
 		});
 	});
+	
+	var dialog = document.querySelector('dialog');
+	var showModalButton = document.querySelector('.show-modal');
+	if (! dialog.showModal) {
+	  dialogPolyfill.registerDialog(dialog);
+	}
+	showModalButton.addEventListener('click', function() {
+	  dialog.showModal();
+	});
+	dialog.querySelector('.close').addEventListener('click', function() {
+	  dialog.close();
+	});
 });
