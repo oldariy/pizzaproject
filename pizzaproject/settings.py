3ef36df2b5f38e4511fa74e45644b5971c4978a4
greypk <greypks@gmail.com>
2017-12-15 00:58:38 +0600

    commit for heroku

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'crc9w*5g03^tiiw^vkvd9d)%r$&bla#o@bvnn$@jd5^5@^)83('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://superpizzaapp.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://superpizzaapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
