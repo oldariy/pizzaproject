b3de30c4a54da9edfd140a7edd01741c15053c64
greypk <greypks@gmail.com>
2017-12-14 23:56:35 +0600

    commit for heroku

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 
