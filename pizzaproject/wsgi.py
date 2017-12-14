f1812fd450355c339070d450481657b8dffa676a
greypk <greypks@gmail.com>
2017-12-14 23:45:45 +0600

    commit for heroku

--- pizzaproject/wsgi.py
+++ pizzaproject/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzaproject.settings")
 
 application = get_wsgi_application()
+
+
+# Use whitenoise to serve static files on heroku
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
